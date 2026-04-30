37869ca46bbcfa6cbfed2034e8d424e1ad25b07b
Jfdeev <jfrsoares161205@gmail.com>
2026-04-30 18:50:49 -0300

    feat: create frists mcp servers

---
+++ src/servers/wheather_server.py
@@ -0,0 +1,12 @@
+from typing import List
+from mcp.server.fastmcp import FastMCP
+
+mcp = FastMCP("Weather")
+
+@mcp.tool()
+def get_weather(city: str) -> str:
+    """Get the current weather for a city."""
+    return "It always rains in Seattle."
+
+if __name__ == "__main__":
+    mcp.run(transport="sse")
